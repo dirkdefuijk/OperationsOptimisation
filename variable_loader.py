65cbefb3f7ab9b45c80b24cf8fc4f9c629306fcc
alvaro98ag <75319201+alvaro98ag@users.noreply.github.com>
2021-11-11 14:49:29 +0100

    Update variable_loader.py

--- variable_loader.py
+++ variable_loader.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 def create_dict(csv_flights, csv_gates):
     dataset_flights = pd.read_csv(csv_flights, header = None, skiprows = [0])
-    flights = {'passengers': dataset_flights.iloc[:,1], 'time period': dataset_flights.iloc[:,2]}
+    flights = {'passengers': dataset_flights.iloc[:,1], 'time period': dataset_flights.iloc[:,2], 'category':dataset_flights.iloc[:,3]}
 
     dataset_gates = pd.read_csv(csv_gates, header = None, skiprows = [0])
-    gates = {'distance': dataset_gates.iloc[:,1]}
+    gates = {'distance': dataset_gates.iloc[:,1], 'category': dataset_gates.iloc[:,2]}
 
     return flights, gates
